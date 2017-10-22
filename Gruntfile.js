a1dc3c396ef74fe531b9ffeb0e7dfa68e1ea7002
esr360 <esr360@live.com>
2017-10-22 23:17:30 +1100

    adding build files

---
+++ Gruntfile.js
@@ -0,0 +1,128 @@
+/******************************************************************
+ * One Nexus
+ * Grunt Setup
+ * @uthor [@esr360](http://twitter.com/esr360)
+ ******************************************************************/
+
+module.exports = function(grunt) {
+
+    // Allows the passing of multiple flags on the command line
+    require('nopt-grunt-fix')(grunt);
+
+    // Import config
+    const CONFIG = require('./build/config')();
+
+    /**************************************************************
+     * Config
+     * @see /build/config.js
+     *************************************************************/
+
+    CONFIG.theme   = grunt.option('theme')   || CONFIG.theme;
+    CONFIG.themes  = grunt.option('themes')  || CONFIG.themes;
+    CONFIG.env     = grunt.option('env')     || CONFIG.env;
+    CONFIG.project = grunt.option('project') || CONFIG.project;
+    CONFIG.src     = grunt.option('src')     || CONFIG.src;
+    CONFIG.dist    = grunt.option('dist')    || CONFIG.dist;
+    CONFIG.scripts = grunt.option('scripts') || CONFIG.scripts;
+    CONFIG.scripts = grunt.option('scripts') || CONFIG.scripts;
+    CONFIG.styles  = grunt.option('styles')  || CONFIG.styles;
+    CONFIG.lint    = grunt.option('lint')    || CONFIG.lint;
+    CONFIG.test    = grunt.option('test')    || CONFIG.test;
+
+    /**************************************************************
+     * Default Tasks
+     * @see /build/tasks/
+     *************************************************************/
+
+    grunt.initConfig({
+        pkg         : grunt.file.readJSON('package.json'),
+        theme       : CONFIG.theme,
+        clean       : require('./build/tasks/clean')(CONFIG),
+        copy        : require('./build/tasks/copy')(CONFIG),
+        sass        : require('./build/tasks/sass')(CONFIG),
+        cssmin      : require('./build/tasks/cssmin')(CONFIG),
+        postcss     : require('./build/tasks/postcss')(CONFIG),
+        csscomb     : require('./build/tasks/csscomb')(CONFIG),
+        browserify  : require('./build/tasks/browserify')(CONFIG),
+        uglify      : require('./build/tasks/uglify')(CONFIG),
+        scsslint    : require('./build/tasks/scsslint')(CONFIG),
+        jshint      : require('./build/tasks/jshint')(CONFIG),
+        mochaTest   : require('./build/tasks/mochaTest')(CONFIG),
+        sassdoc     : require('./build/tasks/sassdoc')(CONFIG),
+        jsdoc       : require('./build/tasks/jsdoc')(CONFIG),
+        assemble    : require('./build/tasks/assemble')(CONFIG),
+        browserSync : require('./build/tasks/browserSync')(CONFIG),
+        watch       : require('./build/tasks/watch')(CONFIG),
+        notify      : require('./build/tasks/notify')(CONFIG)
+    });
+
+    /**************************************************************
+     * Custom Tasks
+     *************************************************************/
+    
+    // Default Grunt task
+    grunt.registerTask('default', [
+        'theme:' + CONFIG.theme,
+        'browserSync',
+        'watch'
+    ]);
+
+    // Compile a specific theme
+    grunt.registerTask('theme', target => {
+        grunt.config('theme', target || CONFIG.theme);
+
+        grunt.task.run(require('./build/compile')({
+            theme: CONFIG.theme,
+            environment: CONFIG.env,
+            lint: CONFIG.lint,
+            test: CONFIG.test,
+        }));
+    });
+
+    // Compile all themes
+    grunt.registerTask('themes', () => {
+        themes.forEach(theme => {
+            grunt.task.run('theme:' + theme);
+        });
+    });
+        
+    // Lint
+    grunt.registerTask('lint', [
+        'scsslint',
+        'jshint'
+    ]);
+        
+    // Test
+    grunt.registerTask('test', [
+        'mochacli:scss'
+    ]);
+
+    // Docs
+    grunt.registerTask('docs', [
+        'sassdoc',
+        'jsdoc'
+    ]);
+    
+    /**************************************************************
+     * Load NPM Tasks
+     *************************************************************/
+    
+    grunt.loadNpmTasks('grunt-assemble');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-csscomb');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-notify');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-sassdoc');
+    grunt.loadNpmTasks('grunt-scss-lint');
+
+};
---
+++ build/config.js
@@ -0,0 +1,70 @@
+///****************************************************************
+/// One-Nexus - A toolkit for architecting and constructing 
+/// front-end user-interfaces - https://github.com/esr360/One-Nexus
+///
+/// @author [@esr360](http://twitter.com/esr360)
+///****************************************************************
+
+module.exports = function config() {
+
+    // Set the default theme to compile assets for
+    this.theme = 'one-nexus';
+
+    // List of all themes used by the project
+    this.themes = [
+        'one-nexus'
+    ];
+        
+    // Set the desired development environment ('dev'|'prod')
+    this.env = 'dev';
+
+    // Lint files when compiling them
+    this.lint = true;
+
+    // Test files when compiling them
+    this.test = true;
+
+    // Map the project's architecture into one Grunt can use
+    // Paths ae relative to the project root
+    this.project = {
+        source: [      'src/', {
+            images:    'src/images/',
+            scripts:   'src/modules/',
+            styles:    'src/modules/',
+            theme:     'src/themes/<%=theme%>/',
+            templates: 'templates/'
+        }],
+        dist: [        'dist/', {
+            images:    'dist/assets/images/',
+            scripts:   'dist/assets/scripts/',
+            styles:    'dist/assets/styles/',
+            theme:     'dist/assets/themes/<%=theme%>/',
+            templates: 'dist/'
+        }],
+        vendor:        'node_modules/',
+        docs: [        'docs/', {
+            scripts:   'docs/js/',
+            styles:    'docs/sass/'
+        }],
+        test: [        'unit-testing/', {
+            scripts:   'unit-testing/js/',
+            styles:    'unit-testing/scss/'
+        }]
+    };
+
+    // Set all optional scripts to be used by the project
+    this.scripts = [];
+
+    // Set all optional styles to be used by the project
+    this.styles = [
+        this.project.vendor + 'flickity/dist/flickity.css'
+    ];
+
+    // The name of your project's source asset
+    this.src = 'app';
+
+    //The name of your project's compiled & distributed asset
+    this.dist = 'app';
+
+    return this;
+}
