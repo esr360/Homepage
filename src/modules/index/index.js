7f949d262f102d30bdfad1707a3ef0c2bb623a69
esr360 <esr360@live.com>
2017-10-30 02:02:53 +1100

    updating homepage

--- src/modules/index/index.js
+++ src/modules/index/index.js
@@ -18,13 +18,15 @@ export function index(els = 'i8', custom = {}) {
         $(window).on('scroll', function() {
             var scrollTop = $(this).scrollTop();
 
-            $('.i8_background').css({
-                'transform': 'translateY(' + scrollTop/-4 + 'px)'
-            });
+            if (app.inViewport({target: document.querySelector('.i8_background')})) {
+                $('.i8_background').css({
+                    'transform': 'translateY(' + scrollTop/-4 + 'px)'
+                });
 
-            $('.i8_foreground').css({
-                'transform': 'translateY(' + scrollTop/-6 + 'px)'
-            });
+                $('.i8_foreground').css({
+                    'transform': 'translateY(' + scrollTop/-6 + 'px)'
+                });
+            }
 
             if (app.media('min-width', 'break-4', app)) {
                 if (app.inViewport({target: document.querySelector('.i8_intro')})) {
@@ -32,9 +34,22 @@ export function index(els = 'i8', custom = {}) {
                         'background-position-y': (0.2 * scrollTop) - 200 + 'px'
                     });
                 }
-                if (app.inViewport({target: document.querySelector('.i8_macbook')})) {
+
+                if (app.inViewport({coverage: 'top', target: document.querySelector('.i8_macbook')})) {
+                    var elem = document.querySelector('.i8_macbook_screen');
+                    var elemTop = (elem.getBoundingClientRect().top - window.innerHeight) + scrollTop;
+
                     $('.i8_macbook_screen img').css({
-                        'transform': 'translateY(' + scrollTop/-1 + 'px)'
+                        'transform': 'translateY(' + ((scrollTop - elemTop) / -0.5) + 'px)'
+                    });
+                }
+                                
+                if (app.inViewport({coverage: 'top', target: document.querySelector('.i8_iphone')})) {
+                    var elem = document.querySelector('.i8_iphone_screen');
+                    var elemTop = (elem.getBoundingClientRect().top - window.innerHeight) + scrollTop;
+
+                    $('.i8_iphone_screen img').css({
+                        'transform': 'translateY(' + ((scrollTop - elemTop) / -0.5) + 'px)'
                     });
                 }
             }
