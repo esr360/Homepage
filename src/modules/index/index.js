b0dd3052908ccaaafcfeeb219091b5afeaa93f2d
esr360 <esr360@live.com>
2017-11-09 00:44:56 +1100

    ajex responses

--- src/modules/index/index.js
+++ src/modules/index/index.js
@@ -13,15 +13,22 @@ export function index(els = 'i8', custom = {}) {
 
     custom = app.custom('index', custom);
 
-    const DRIBBBLE_TOKEN = '4526e22a9e300d869d7150623fb27351bd4e80aaafa2cebed0429e5a6e395e22';
     const NEW_RANGE = Date.now() - 2592000000;
+    const header_newSocial_item_template = document.getElementById('header_newSocial_item_template').innerHTML;
 
-    app.Synergy(els, (el, options) => {    
-        // Populate Dribbble shots
+    app.Synergy(els, (el, options) => {
+        // Fetch Dribbble shots
         $.ajax({
             type: 'GET',
-            url: 'https://api.dribbble.com/v1/users/esr360/shots/?access_token=' + DRIBBBLE_TOKEN,
+            url: 'https://api.dribbble.com/v1/users/esr360/shots/?access_token=' + options['dribbble-token'],
             success: response => this.handleDribbbleShots(response, '#dribbble_shots')
+        });
+        
+        // Fech Github Info
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.github.com/users/esr360/repos',
+            success: response => this.handleGithubRepos(response)
         });
 
         // Parallax
@@ -45,7 +52,7 @@ export function index(els = 'i8', custom = {}) {
                     });
                 }
 
-                if (app.inViewport({coverage: 'top', target: document.querySelector('.i8_macbook')})) {
+                if (app.inViewport({target: document.querySelector('.i8_macbook'), coverage: 'top'})) {
                     var elem = document.querySelector('.i8_macbook_screen');
                     var elemTop = (elem.getBoundingClientRect().top - window.innerHeight) + scrollTop;
 
@@ -54,7 +61,7 @@ export function index(els = 'i8', custom = {}) {
                     });
                 }
                 
-                if (app.inViewport({coverage: 'top', target: document.querySelector('.i8_iphone')})) {
+                if (app.inViewport({target: document.querySelector('.i8_iphone'), coverage: 'top'})) {
                     var elem = document.querySelector('.i8_iphone_screen');
                     var elemTop = (elem.getBoundingClientRect().top - window.innerHeight) + scrollTop;
 
@@ -79,29 +86,63 @@ export function index(els = 'i8', custom = {}) {
             target = document.querySelector(target);
         }
 
-        const header_icon = $('.header_social-links_item[title=Dribbble]');
+        const wrapper = document.querySelector('.i8_newSocial_dribbble');
+        const shotTemplate = document.getElementById('i8_dribbble_shot_template').innerHTML;
 
         let newPosts = 0;
         
         shots.forEach(shot => {
             const isNew = Date.parse(shot.created_at) > NEW_RANGE;
-            const shotColumnClass = `span-3 break-3-third break-2-half break-1-full${isNew ? ' new' : ''}`;
 
-            if (isNew) newPosts++;
+            if (isNew) {
+                newPosts++;
+
+                wrapper.querySelector('.header_newSocial').insertAdjacentHTML('beforeend',  
+                    header_newSocial_item_template
+                        .replace('${shot.title}', shot.title)
+                        .replace('${shot.description}', shot.description)
+                        .replace('${shot.url}', shot.html_url)
+                );
+            }
 
             target.insertAdjacentHTML('beforeend', 
-                `<a target="blank" href="${shot.html_url}" class="i8_dribbble_shot ${shotColumnClass}">
-                    <img class="" src="${shot.images.normal}" />
-                </a>`
+                shotTemplate
+                    .replace('${shot.html_url}', shot.html_url)
+                    .replace('${shot.images.normal}', shot.images.normal)
             );
         });
 
         if (newPosts) {
-            header_icon.addClass('new');
-            header_icon.attr('data-new-posts', newPosts);
+            $('.header_social-links_item[title=Dribbble]').addClass('new').attr('data-new-posts', newPosts);
         }
 
         if (app.media('min-width', 'break-4', app)) app.Tilt();  
+    }
+    
+    this.handleGithubRepos = (repos) => {
+
+        const wrapper = document.querySelector('.i8_newSocial_github');
+
+        let updates = 0;
+
+        repos.forEach(repo => {
+            if (repo.owner.login === 'esr360') {
+                if (Date.parse(repo.updated_at) > NEW_RANGE) {
+                    updates++;
+                    
+                    wrapper.querySelector('.header_newSocial').insertAdjacentHTML('beforeend',  
+                        header_newSocial_item_template
+                            .replace('${shot.title}', shot.title)
+                            .replace('${shot.description}', shot.description)
+                            .replace('${shot.url}', shot.html_url)
+                    );
+                }
+            }
+        });
+        
+        if (updates) {
+            $('.header_social-links_item[title=Github]').addClass('new').attr('data-new-posts', updates);
+        }
     }
 
     app.config.index = app.parse(defaults.index, custom);
